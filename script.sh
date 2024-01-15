8b20341b726bf86731bfab806979c1284a83f28f
JESUS VILLALOBOS <jveyes@gmail.com>
2024-01-15 09:28:03 -0500

    OK

--- script.sh
+++ script.sh
@@ -29,6 +29,21 @@ execute_command3() {
     # Add your command here
 }
 
+# Function to start task
+task_started() {
+    echo -e "╭─────────────────────────────────────────────╮"
+    echo -e "│ \e[93mTHE TASK HAS STARTED RUNNING\e[0m                │"
+    echo -e "╰─────────────────────────────────────────────╯"
+}
+
+# Function to end task
+task_ended() {
+    echo ""
+    echo -e "╭─────────────────────────────────────────────╮"
+    echo -e "│ \e[32mTHE TASK HAS FINISHED RUNNING\e[0m               │"
+    echo -e "╰─────────────────────────────────────────────╯"
+}
+
 # Main loop for the menu
 while true; do
     display_menu
@@ -37,40 +52,34 @@ while true; do
 
     case $choice in
         1)
-            echo -e "╭─────────────────────────────────────────────╮"
-            echo -e "│ \e[93mTHE TASK HAS STARTED RUNNING\e[0m                │"
-            echo -e "╰─────────────────────────────────────────────╯"
+            task_started
             execute_command1
+            task_ended
             ;;
         2)
-            echo -e "╭─────────────────────────────────────────────╮"
-            echo -e "│ \e[93mTHE TASK HAS STARTED RUNNING\e[0m                │"
-            echo -e "╰─────────────────────────────────────────────╯"
+            task_started
             execute_command2
+            task_ended
             ;;
         3)
-            echo -e "╭─────────────────────────────────────────────╮"
-            echo -e "│ \e[93mTHE TASK HAS STARTED RUNNING\e[0m                │"
-            echo -e "╰─────────────────────────────────────────────╯"    
+            task_started
             execute_command3
+            task_ended
             ;;
         4)
             echo ""
-            echo -e "╭─────────────────────────────────────────────╮"
-            echo -e "│ \e[31mExiting the menu. Goodbye!\e[0m                  │"
-            echo -e "╰─────────────────────────────────────────────╯"
+            echo -e "\e[32mExiting the menu. Goodbye!\e[0m"
             echo ""
             exit 0
             ;;
         *)
             echo ""
-            echo -e "\e[31mInvalid choice. Please enter a number between 1 and 4.\e[0m"
+            echo -e "\e[31mInvalid choice [\e[1m$choice\e[0m\e[31m]\e[0m"
+            echo -e "\e[31mPlease enter a number between 1 and 4.\e[0m"
+            echo ""
             ;;
     esac
 
-    echo ""
-    echo -e "╭─────────────────────────────────────────────╮"
-    echo -e "│ \e[32mTHE TASK HAS FINISHED RUNNING\e[0m               │"
-    echo -e "╰─────────────────────────────────────────────╯"
-    read -p "Press Enter to continue..."
+    echo -e -n "\e[1mPress Enter to continue...\e[0m"
+    read -s -n 1 -p ""
 done

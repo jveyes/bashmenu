788c22e256e7a41b55a026df84ae88bf13c09e4c
JESUS VILLALOBOS <jveyes@gmail.com>
2024-04-16 14:24:40 -0500

    2024-04-16 14:24:40

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ SERVER="ubuntu@test.jemavi.co"
 REMOTE_PATH="/home/ubuntu/got/bashmenu"
 LOCAL_PATH="/home/stk/GIT/bashmenu"
 cd "$LOCAL_PATH"
-git pull
 ssh "$SERVER" "cd '$REMOTE_PATH' && git pull"
 git add .
 git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"

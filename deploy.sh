d9616e2ac4b054980cf94fc849b9db881b7345e8
JESUS VILLALOBOS <jveyes@gmail.com>
2024-04-16 14:22:29 -0500

    OK

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set the server details
+SERVER="ubuntu@test.jemavi.co"
+REMOTE_PATH="/home/ubuntu/got/bashmenu"
+LOCAL_PATH="/home/stk/GIT/bashmenu"
+cd "$LOCAL_PATH"
+git pull
+ssh "$SERVER" "cd '$REMOTE_PATH' && git pull"
+git add .
+git commit -m "OK"

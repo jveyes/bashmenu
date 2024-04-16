366500c9d506a8dee22796c424f2dfbf19d0fe34
JESUS VILLALOBOS <jveyes@gmail.com>
2024-04-16 14:24:05 -0500

    2024-04-16 14:24:05

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,6 @@ cd "$LOCAL_PATH"
 git pull
 ssh "$SERVER" "cd '$REMOTE_PATH' && git pull"
 git add .
-git commit -m "OK"
+git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"
+git push
+echo $(date +"%Y-%m-%d %H:%M:%S")

62a8a76cf4d99eb349b1d511b066e9c15f2ff32b
JESUS VILLALOBOS <jveyes@gmail.com>
2024-04-16 20:01:12 -0500

    2024-04-16 20:01:12

--- serport.sh
+++ serport.sh
@@ -1,60 +1,67 @@
 #!/bin/bash
 
 # Set the server details
+
 SERVER="ubuntu@test.jemavi.co"
+
 REMOTE_PATH="/home/ubuntu/got/bashmenu"
+
 LOCAL_PATH="/home/stk/GIT/bashmenu"
 
 # Function to display a progress bar
+
 display_progress_bar() {
-    local progress=$1
-    local total_length=20
-    local filled_length=$((total_length * progress / 100))
-    local empty_length=$((total_length - filled_length))
 
-    printf "\r[%-${total_length}s] %3d%%  " \
-           $(printf "#%.0s" $(seq 1 $filled_length)) \
-           $progress
-}
+local progress=$1
 
-# Function to display status messages with colors and a frame
-display_status() {
-    local color=$1
-    local message=$2
-    local frame_length=$((${#message} + 4))
-    local frame_char="─"
+local total_length=20
 
-    tput bold
-    tput setaf "$color"
-    echo "╭──────────────────────────────────────────────────────────╮"
-    printf "│ %s                   │\n" "$message"
-    echo "╰──────────────────────────────────────────────────────────╯"
-    tput sgr0
+local filled_length=$((total_length * progress / 100))
+
+local empty_length=$((total_length - filled_length))
+
+printf "\r[%-${total_length}s] %3d%% " \
+
+$(printf "#%.0s" $(seq 1 $filled_length)) \
+
+$progress
+
 }
 
 # Change to the local repository directory
+
 cd "$LOCAL_PATH"
 
 # Add and commit the changes
-display_status 34 "Adding all changes to the local server...\e[0m"
+
+echo -e "\e[1;34mAdding all changes to the local server...\e[0m"
+
 git add .
-echo -e "\e[1;32mCommitting changes with timestamp..."
+
+echo -e "\e[1;32mCommitting changes with timestamp...\e[0m"
+
 git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"
+
 git push
 
 # Push the changes to the remote repository
-echo -e "\e[1;32mPushing changes to the github repository...\e[0m"
+
+echo -e "\e[1;34mPushing changes to the github repository...\e[0m"
+
 for i in {1..100}; do
-    display_progress_bar $i
-    sleep 0.05
+
+display_progress_bar $i
+
+sleep 0.05
+
 done
-display_status 34 "Push successful to github!"
+
+echo -e "\e[1;32mPush successful to github!\e[0m"
 
 # Pull the changes on the remote repository
-echo -e "\e[1;32mPulling changes from the github repository...\e[0m"
+
+echo -e "\e[1;34mPulling changes from the github repository...\e[0m"
+
 ssh "$SERVER" "cd '$REMOTE_PATH' && git pull"
-for i in {1..100}; do
-    display_progress_bar $i
-    sleep 0.05
-done
-display_status 34 "Pull successful from github!"
+
+echo -e "\e[1;32mPull successful from github!\e[0m"
